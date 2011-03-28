81dc1a03ecc7aa397f703cdea39a43f12d29e80a
unknown <Joe@.(none)>
2011-03-28 19:24:42 +0100

    Added basic `INI.Secton` struct, which allows you to restrict to a particular `[Section]` of the INI file.

--- fbstj/INI.cs
+++ fbstj/INI.cs
@@ -7,6 +7,14 @@ namespace fbstj
 {
     public struct INI
     {
+        public struct Section
+        {
+            private INI _ini;
+            private string _section;
+            internal Section(INI ini, string section) { _ini = ini; _section = section; }
+            public string this[string key] { get { return _ini[_section, key]; } set { _ini[_section, key] = value; } }
+        }
+
         [DllImport("kernel32")]
         private static extern long WritePrivateProfileString(string s, string k, string v, string f);
         [DllImport("kernel32")]
@@ -26,5 +34,7 @@ namespace fbstj
             }
             set { WritePrivateProfileString(section, key, value, _path); }
         }
+
+        public Section this[string section] { get { return new Section(this, section); } }
     }
 }
