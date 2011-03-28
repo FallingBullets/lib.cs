996350e1456380dee78f275a25c093f7f1b7f829
unknown <Joe@.(none)>
2011-03-28 19:16:18 +0100

    Initial Commit (empty DLL project + testing project

---
+++ fbstj.test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace fbstj.test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test();
+            while (true) ;
+        }
+
+
+        static void test()
+        {
+            Console.WriteLine("testing");
+        }
+    }
+}
