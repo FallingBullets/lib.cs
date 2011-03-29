221b12f5966c9f19a35dab8881f0ccce0f10590c
Joe ST <joe@fbstj.net>
2011-03-29 23:34:28 +0100

    Added `fbstj.math.Angle` which can:  - convert between {degrees,radians,gradians,percentages}  - normalize an angle around 0 or between 0 and the period.

--- fbstj.test/Program.cs
+++ fbstj.test/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using fbstj;
+using fbstj.math;
 
 namespace fbstj.test
 {
@@ -7,9 +9,16 @@ namespace fbstj.test
         static void Main(string[] args)
         {
             test();
+            for (int i = -540; i < 540; i += 10)
+                Console.WriteLine(printAngle(i, Angle.Unit.RADIANS));
             while (true) ;
         }
 
+        static string printAngle(double a, Angle.Unit u)
+        {
+            Angle _a = new Angle(a, Angle.Unit.DEGREES);
+            return _a[Angle.Unit.DEGREES] + ":" + _a.Normalised(u, true);
+        }
 
         static void test()
         {
---
+++ fbstj/math/Angle.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fbstj.math
+{
+    public struct Angle
+    {
+        #region static
+        /// <summary>A set of angular units</summary>
+        public enum Unit
+        {
+            /// <summary>360 degrees in a circle</summary>
+            DEGREES,
+            /// <summary>2*PI radians in a circle</summary>
+            RADIANS,
+            /// <summary>400 gradians in a circle</summary>
+            GRADIANS,
+            /// <summary>100% of the circle</summary>
+            PERCENTILES
+        }
+
+        /// <summary>The absolute period of a unit</summary>
+        /// <param name="unit">An angular unit</param>
+        /// <returns>The number of units in a full circle</returns>
+        public static double Period(Unit unit)
+        {
+            switch (unit)
+            {
+                case Unit.DEGREES:
+                    return 360;
+                case Unit.GRADIANS:
+                    return 400;
+                case Unit.PERCENTILES:
+                    return 100;
+                case Unit.RADIANS:
+                    return Math.PI * 2;
+                default:
+                    return 1;
+            }
+        }
+
+        /// <summary>The upper bound of a unit</summary>
+        /// <param name="unit">An angular unit</param>
+        /// <param name="zero">Center the period around 0</param>
+        /// <returns>The maximum value that ther period take</returns>
+        public static double Max(Unit unit, bool zero) { return Period(unit) / (zero ? 2 : 1); }
+
+        /// <summary>A minimum period</summary>
+        /// <param name="unit">The periods unit</param>
+        /// <param name="zero">Center the period around 0</param>
+        /// <returns>The minimum value that the period takes</returns>
+        public static double Min(Unit unit, bool zero) { return zero ? -Period(unit) / 2 : 0; }
+
+        /// <summary>The ratio between two units, used for converting between them.</summary>
+        /// <param name="from">The old unit</param>
+        /// <param name="to">The new unit</param>
+        /// <returns>The ratio converting old units to new units</returns>
+        public static double Ratio(Unit from, Unit to) { return (from == to) ? 1 : Period(to) / Period(from); }
+        #endregion
+
+        #region state
+        private double _value;
+        private Unit _unit;
+
+        public Angle(double value, Unit unit) : this() { _value = value; _unit = unit; }
+
+        public double this[Unit unit] { get { return _value * Ratio(_unit, unit); } }
+
+        public double Normalised(Unit unit, bool aroundZero)
+        {
+            double _a = this[unit];
+            double _p = Period(unit), _max = Max(unit, aroundZero), _min = Min(unit, aroundZero);
+            while (_a >= _max) _a -= _p;
+            while (_a < _min) _a += _p;
+            return _a;
+        }
+        #endregion
+    }
+}
