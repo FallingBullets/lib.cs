9b4d89284775d2bae2afb5f45bf5a844c0a7c28f
unknown <Joe@.(none)>
2011-03-29 19:33:10 +0100

    Moved Get and Set to private static functions  - Removed indirection in `Section.this[]`. Added some comments

--- fbstj/INI.cs
+++ fbstj/INI.cs
@@ -5,36 +5,54 @@ using System.Runtime.InteropServices;
 
 namespace fbstj
 {
+    /// <summary>An INI file</summary>
     public struct INI
     {
-        public struct Section
-        {
-            private INI _ini;
-            private string _section;
-            internal Section(INI ini, string section) { _ini = ini; _section = section; }
-            public string this[string key] { get { return _ini[_section, key]; } set { _ini[_section, key] = value; } }
-        }
-
+        #region static
         [DllImport("kernel32")]
         private static extern long WritePrivateProfileString(string s, string k, string v, string f);
         [DllImport("kernel32")]
         private static extern int GetPrivateProfileString(string s, string k, string def, StringBuilder v, int size, string f);
 
-        private readonly string _path;
-
-        public INI(string path) { _path = path; }
-
-        public string this[string section, string key]
+        private static string Get(params string[] psk)
         {
-            get
-            {
-                StringBuilder temp = new StringBuilder(255);
-                int i = GetPrivateProfileString(section, key, "", temp, 255, _path);
-                return temp.ToString();
-            }
-            set { WritePrivateProfileString(section, key, value, _path); }
+            StringBuilder temp = new StringBuilder(255);
+            int i = GetPrivateProfileString(psk[1], psk[1], "", temp, 255, psk[0]);
+            return temp.ToString();
         }
 
+        private static void Set(params string[] pskv)
+        {
+            WritePrivateProfileString(pskv[1], pskv[2], pskv[3], pskv[0]);
+        }
+
+        /// <summary>A named section of an INI file</summary>
+        public struct Section
+        {
+            /// <summary>The path to the INI file</summary>
+            public readonly string File;
+
+            /// <summary>The section heading</summary>
+            public readonly string Name;
+
+            internal Section(INI ini, string section) { Name = section; File = ini.File; }
+
+            /// <summary></summary>
+            public string this[string key] { get { return Get(File, Name, key); } set { Set(File, Name, key, value); } }
+        }
+        #endregion
+
+        #region state
+        /// <summary>The path to the INI file</summary>
+        public readonly string File;
+
+        public INI(string path) { File = path; }
+
+        /// <summary>The value of a key under an INI section.</summary>
+        internal string this[string section, string key] { get { return Get(File, section, key); } set { Set(File, section, key, value); } }
+
+        /// <summary>An INI section</summary>
         public Section this[string section] { get { return new Section(this, section); } }
+        #endregion
     }
 }
