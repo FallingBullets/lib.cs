94dcbe85e5d6509225b67308b9c69e4a3ad6d3e8
Joe ST <joe@fbstj.net>
2011-04-22 15:05:55 +0100

    Added INI.Section struct, which allows you to use a `[section]` of an ini file. Comments

--- INI.cs
+++ INI.cs
@@ -7,6 +7,29 @@ namespace fbstj
 {
     public struct INI
     {
+        #region section
+        public struct Section
+        {
+            /// <summary>The storage location</summary>
+            public readonly string Path;
+
+            /// <summary>The section heading</summary>
+            public readonly string Name;
+
+            internal Section(INI parent, string section) { Path = parent.Path; Name = section; }
+
+            /// <summary>The value of a key under this INI section</summary>
+            public string this[string key]
+            {
+                get { return Get(Path,Name,key); }
+                set { Set(Path, Name, key, value); }
+            }
+
+            public override string ToString() { return "[" + Name + "]"; }
+        }
+        #endregion
+
+        #region static
         [DllImport("kernel32")]
         private static extern long WritePrivateProfileString(string s, string k, string v, string f);
 
@@ -24,6 +47,23 @@ namespace fbstj
         {
             WritePrivateProfileString(section, key, value, path);
         }
+        #endregion
 
+        #region state
+        /// <summary>The storage location</summary>
+        public readonly string Path;
+
+        public INI(string path) { Path = path; }
+
+        /// <summary>The value of a key under an INI section</summary>
+        public string this[string section, string key]
+        {
+            get { return Get(Path, section, key); }
+            set { Set(Path, section, key, value); }
+        }
+
+        /// <summary>An INI section</summary>
+        public Section this[string section] { get { return new Section(this, section); } }
+        #endregion
     }
 }
