f8f207332b083ff3f13db904305c06bb3e654414
Joe ST <joe@fbstj.net>
2011-10-26 20:13:50 +0100

    [Polynomial]  - add _init method so state is initialised correctly  - protect against nonexistant key retrieval (for +=)  - add Polynomial + Polynomial operator  - add ToString method for pretty printing

--- Polynomial.cs
+++ Polynomial.cs
@@ -5,7 +5,20 @@ namespace fbstj
 {
 	public struct Polynomial
 	{
+		public static Polynomial operator +(Polynomial p, Polynomial q)
+		{
+			var v = new Polynomial();
+			p._init(); q._init();
+			foreach(var x in p._co)
+				v[x.Key] = x.Value;
+			foreach(var y in q._co)
+				v[y.Key] += y.Value;
+			return v;
+		}
+
 		private Dictionary<int,double> _co;
+
+		private void _init() { if(_co == null) _co = new Dictionary<int,double>(); }
 
 		/// <summary>
 		/// Constructs a polynomial starting from a non-zero term
@@ -27,9 +40,10 @@ namespace fbstj
 
 		public double this[int i]
 		{
-			get { return _co[i]; }
+			get { _init(); return _co.ContainsKey(i)?_co[i]:0; }
 			set
 			{
+				_init();
 				if(_co.ContainsKey(i))
 					_co[i] = value;
 				else
@@ -43,10 +57,37 @@ namespace fbstj
 		/// <param name="at">The value of 'x' to evaluate at</param>
 		public double Evaluate(double at)
 		{
+			_init();
 			double y = 0;
 			foreach(var term in _co)
 				y += Math.Pow(at, term.Key) * term.Value;
 			return y;
+		}
+
+		public override string ToString()
+		{
+			_init();
+			var terms = new List<string>();
+			foreach(var term in _co)
+			{
+				string x;
+				if(term.Value == 0)
+					continue;
+				switch(term.Key)
+				{
+					case 0:
+						x = "";
+						break;
+					case 1:
+						x =  "x";
+						break;
+					default:
+						x = "x^" + term.Key;
+						break;
+				}
+				terms.Add(term.Value + x);
+			}
+			return String.Join(" + ", terms.ToArray());
 		}
 	}
 }
