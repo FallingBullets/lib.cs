9b9f95546b72807f80a83f1885a2b6fac39c56eb
JoeST <joe@fbstj.net>
2011-10-26 12:56:28 +0200

    Add method comments

--- Polynomial.cs
+++ Polynomial.cs
@@ -7,6 +7,11 @@ namespace fbstj
 	{
 		private Dictionary<int,double> _co;
 
+		/// <summary>
+		/// Constructs a polynomial starting from a non-zero term
+		/// </summary>
+		/// <param name="start">The index of the first power of x in the polynomial</param>
+		/// <param name="coefficients">The coefficients a_i</param>
 		public Polynomial(int offset, params double[] coefficients)
 		{
 			_co = new Dictionary<int,double>();
@@ -14,6 +19,10 @@ namespace fbstj
 				this[offset++] = co;
 		}
 
+		/// <summary>
+		/// Constructs a polynomial a_0 + (a_1 * x) + (a_2 * x^2) + ... + (a_n * x^n)
+		/// </summary>
+		/// <param name="coefficients">The coefficients a_i</param>
 		public Polynomial(params double[] coefficients) : this(0, coefficients) {  }
 
 		public double this[int i]
@@ -28,6 +37,10 @@ namespace fbstj
 			}
 		}
 
+		/// <summary>
+		/// Evaluate the equation at a particular value of 'x'
+		/// </summary>
+		/// <param name="at">The value of 'x' to evaluate at</param>
 		public double Evaluate(double at)
 		{
 			double y = 0;
