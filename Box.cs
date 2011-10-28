ef0e40229676e61cfd740343b4a3daa79229c727
Joe ST <joe@fbstj.net>
2011-10-28 20:03:22 +0100

    [Box] now supports differently typed axes  - add Tx and Ty generic types  - fix up function parameters and Span state

--- Box.cs
+++ Box.cs
@@ -3,44 +3,41 @@ using System.Collections.Generic;
 
 // reqires Span.cs
 
-/* Span<T> - span/range/interval structure.
- *
- * Allows you to do some simple interval algebra.
- * Similar to DateTime/TimeSpan but for any IComparable
- */
 namespace fbstj
 {
-	public struct Box<T>
-		where T : IComparable<T>
+	public struct Box<Tx, Ty>
+		where Tx : IComparable<Tx>
+		where Ty : IComparable<Ty>
 	{
-		public static Box<T> Get(T x1, T y1, T x2, T y2)
+		public static Box<Tx, Ty> Get(Tx x1, Ty y1, Tx x2, Ty y2)
 		{
-			Box<T> b = default(Box<T>);
-			b.X = new Span<T>(x1, x2);
-			b.Y = new Span<T>(y1, y2);
+			Box<Tx,Ty> b = default(Box<Tx,Ty>);
+			b.X = new Span<Tx>(x1, x2);
+			b.Y = new Span<Ty>(y1, y2);
 			return b;
 		}
 
-		public static Box<T> Bounds(params Box<T>[] boxes)
+		public static Box<Tx,Ty> Bounds(params Box<Tx,Ty>[] boxes)
 		{
-			Box<T> b = boxes[0];
+			Box<Tx,Ty> b = boxes[0];
 			foreach(var box in boxes)
 			{
-				b.X = new Span<T>(b.X.Maximum, box.X.Maximum, b.X.Minimum, box.X.Minimum);
-				b.Y = new Span<T>(b.Y.Maximum, box.Y.Maximum, b.Y.Minimum, box.Y.Minimum);
+				b.X = new Span<Tx>(b.X.Maximum, box.X.Maximum, b.X.Minimum, box.X.Minimum);
+				b.Y = new Span<Ty>(b.Y.Maximum, box.Y.Maximum, b.Y.Minimum, box.Y.Minimum);
 			}
 			return b;
 		}
 
-		private Span<T> X, Y;
+		private Span<Tx> X;
+		private Span<Ty> Y;
 
 		public bool Empty { get { return X.Empty && Y.Empty; } }
 
-		public bool Contains(T x, T y) { return X.Contains(x) && Y.Contains(y); }
+		public bool Contains(Tx x, Ty y) { return X.Contains(x) && Y.Contains(y); }
 
-		public bool Intersects(Box<T> box) { return X.Intersects(box.X) && Y.Intersects(box.Y); }
+		public bool Intersects(Box<Tx,Ty> box) { return X.Intersects(box.X) && Y.Intersects(box.Y); }
 
-		public bool Contains(Box<T> box) { return X.Contains(box.X) && Y.Contains(box.Y); }
+		public bool Contains(Box<Tx,Ty> box) { return X.Contains(box.X) && Y.Contains(box.Y); }
 
 		public override string ToString(){ return "{x:" + X + ",y:" + Y + "}"; }
 	}
