eaa558e86dd872e8a70d29aa35f5730639cdcb62
Joe Dowswell <joe@radix2design.co.uk>
2012-03-07 15:13:23 +0000

    [fbstj.Serial] add CAN things  - move NMEA into ./Serial/  - move Converter<F,T> to Serial.cs  - rename Consumer<T> to Reciever<T> and move to serial.cs  - add Serial.Transport<T> interface  - add Serial.CAN.Frame struct and CAN.Port interface [project] add all classes to project for compilation

---
+++ Serial/CAN.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fbstj.Serial.CAN
+{
+	/// <summary>A timestamped receiver</summary>
+	public delegate void TimedReciever(TimeSpan at, Frame f);
+
+	/// <summary>A CAN port interface</summary>
+	public interface Port : Serial.Transport<Frame>
+	{
+		/// <summary>Distributes a recieved frame along with a TimeSpan timestamp</summary>
+		event TimedReciever TimedReceive;
+	}
+
+	/// <summary>A CAN frame</summary>
+	public struct Frame
+	{
+		/// <summary>The frame identifier</summary>
+		public int ID;
+		/// <summary>The frame has a 29-bit address</summary>
+		public bool Extended;
+		/// <summary>The frame is a Remote-Transfer-Request</summary>
+		public bool Remote;
+		/// <summary>The number of bytes in </summary>
+		public byte Length
+		{
+			get { return (byte)_data.Length; }
+			set { Array.Resize<byte>(ref _data, value); }
+		}
+		/// <summary></summary>
+		public byte this[int i] { get { return _data[i]; } set { _data[i] = value; } }
+		private byte[] _data;
+
+		public override string ToString()
+		{
+			return "ID="+ID.ToString("X")+",Data=";
+		}
+	}
+}
---
+++ Serial/Serial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace fbstj.Serial
+{
+	public delegate void Receiver<T>(T f);
+	public delegate T Converter<F,T>(F o);
+
+	public interface Transport<T>
+	{
+		event Receiver<T> Receive;
+
+		void Send(T t);
+	}
+}
