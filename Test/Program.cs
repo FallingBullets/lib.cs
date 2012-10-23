173523db19916d516587c041f70a63ba26efc1aa
Joe Dowswell <joe@radix2design.co.uk>
2012-10-23 10:41:21 +0100

    [test] add test application

---
+++ Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Algebra.Extensions;
+
+namespace Algebra
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var set = SetExtensions.Set<int>(1, 2, 3);
+			var power = set.Powerset();
+			foreach (var tuple in set.TuplesOf(5))
+				Console.WriteLine("[" + String.Join(",", tuple) + "]");
+		}
+	}
+}
