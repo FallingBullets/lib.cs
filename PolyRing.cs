7caef65ec909c58efa214743ba9f8a6b401a65fc
Joe Dowswell <joe@radix2design.co.uk>
2013-01-29 10:11:41 +0000

    [PolyRing] evaluate on Dictionary<K,V>

--- PolyRing.cs
+++ PolyRing.cs
@@ -1,47 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace fbstj
 {
-	/// <summary>A binary opperation on T</summary>
-	public delegate T BinOp<T>(T a, T b);
-	/// <summary>The value of x 'to the power of' e</summary>
-	public delegate T PowOp<T>(T x, int e);
-
-	public struct PolynomialRing<T>
-		where T : struct
+	public static class RingExtensions
 	{
-		private List<T> _co;
-
-		/// <summary>Constructs an abstract polynomial</summary>
-		public PolynomialRing(params T[] coefficients)
+		public static Tv EvaluatePolynomial<Ti, Tv>(this IDictionary<Ti, Tv> _, Tv at, Func<Tv, Tv, Tv> add, Func<Tv, Tv, Tv> prod, Func<Tv, Ti, Tv> pow)
 		{
-			_co = new List<T>(coefficients);
-		}
-
-		/// <summary>The degree of the polynomial</summary>
-		public int Degree { get { return _co.Count; } }
-
-		/// <summary>The coefficient of a term in the polynomial</summary>
-		public T this[int i]
-		{
-			get { return _co[i]; }
-			set
-			{
-				if (_co.Count > i)
-					_co[i] = value;
-				else
-					_co.Add(value);
-			}
-		}
-
-		/// <summary>Evaluate the polynomial at a specific point</summary>
-		/// add and prod are the binary operators which form a Polynomial Ring over `T`
-		/// pow is a function that takes a T to a positive power
-		public T Evaluate(T at, BinOp<T> add, BinOp<T> prod, PowOp<T> pow)
-		{
-			T y = default(T);
-			for (int i = 0; i < Degree; i++)
-				y = add(y, prod(this[i], pow(at, i)));
+			Tv y = default(Tv);
+			foreach (var k in _.Keys)
+				y = add(y, prod(_[k], pow(at, k)));
 			return y;
 		}
 	}
